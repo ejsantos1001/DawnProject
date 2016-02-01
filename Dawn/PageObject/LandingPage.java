b7ebe428ea8d89a133da048bd50849d9a599a044
ej santos <eliazersantos.mail@gmail.com>
2016-02-01 20:25:26 +0800

    First commit

---
+++ Dawn/PageObject/LandingPage.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package PageObject;
+
+/**
+ * @author ejsantos
+ *
+ */
+public class LandingPage {
+	WebDriver driver;
+	
+	public LandingPage(driver) {
+		this.driver = driver;
+		
+	}
+	
+	private WebElement SignInButton;
+	
+
+}
