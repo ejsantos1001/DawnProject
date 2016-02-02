0a9da1abf4fe87088b73d41a141735dc1561d662
ej santos <eliazersantos.mail@gmail.com>
2016-02-02 23:07:19 +0800

    Standardized PageObject Model

---
+++ Dawn/src/stg/angloinfo/tests/TestObjectA.java
@@ -0,0 +1,31 @@
+package stg.angloinfo.tests;
+
+import org.testng.annotations.Test;
+
+import stg.angloinfo.boilerplates.BaseTest;
+import stg.angloinfo.pageobjects.IndexPage;
+import stg.angloinfo.pageobjects.MemberDashBoardPage;
+import stg.angloinfo.pageobjects.SignInPage;
+import org.testng.annotations.DataProvider;
+
+
+
+
+public class TestObjectA extends BaseTest {
+
+  @Test(dataProvider ="dp")
+  public void TestAction(String email, String password) {
+	  IndexPage IndexPageInstance = new IndexPage(driver);
+	  SignInPage SignInPageInstance = IndexPageInstance.goToSignInPage();
+	  MemberDashBoardPage MemberDashBoardInstance = SignInPageInstance.SignInWithEmail(email, password);
+	  
+  }
+;
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { "eliazertest+5@gmail.com", "Ai1234567" },
+      //new Object[] { 2, "b" },
+    };
+  }
+}
